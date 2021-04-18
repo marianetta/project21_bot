1b8b2760bf7f7df122de755aedff5baf939c177f
ioiimm <71763276+ioiimm@users.noreply.github.com>
2021-04-18 15:13:47 +0300

    Update test_fickle_join.py

--- test_fickle_join.py
+++ test_fickle_join.py
@@ -2,7 +2,7 @@ import telebot
 from requests import get
 from telebot import types
 
-bot = telebot.TeleBot('1751134716:AAHHDwQ1SW5gTSunprNygu-Q7EQh4KSesEY')
+bot = telebot.TeleBot('1606668575:AAFLv5Vh3_2EURVsVzaH3hYEVDCja8lgOmU')  # личный бот!!
 
 keyboard = telebot.types.InlineKeyboardMarkup()
 key_memes = telebot.types.InlineKeyboardButton(text='Мемы', callback_data='memes')
@@ -42,6 +42,7 @@ def callback_worker(call):
     elif call.data == 'organisation':
         bot.send_message(call.message.chat.id, '''В этом разделе будут вопросы, связанные с учебным процессом на фикле. 
 Выберите верный вариант ответа или введите слово.''')
+
         keyboard_org = types.ReplyKeyboardMarkup(False, True)
         key_var1 = types.KeyboardButton("фонетика")
         key_var2 = types.KeyboardButton("социолингвистика")
@@ -63,7 +64,7 @@ def callback_worker(call):
                 bot.send_message(org1.chat.id, "Как зовут Ландера?", reply_markup=markup)
                 bot.register_next_step_handler(r, callback_worker_org2)
             elif org1.text == 'социолингвистика' or org1.text == 'академическое письмо' or org1.text == 'пары по языкам':
-                r = bot.send_message(org1.chat.id, 'Нет(')
+                r = bot.send_message(org1.chat.id, 'Нет( \nправильный ответ: фонетика')
                 markup = types.ReplyKeyboardMarkup(False, True)
                 itembtna = types.KeyboardButton('Юрий Александрович')
                 itembtnv = types.KeyboardButton('Михаил Александрович')
@@ -78,11 +79,64 @@ def callback_worker(call):
                               reply_markup=keyboard_org)
         bot.register_next_step_handler(r1, callback_worker_org)
 
+        def callback_worker_org5(org5):
+            if org5.text == "лесбистки(-ы)’20":
+                bot.send_message(org5.chat.id, "Да!")
+
+            else:
+                bot.send_message(org5.chat.id, "Нет( \nправильный ответ: лесбистки(-ы)’20")
+
+        def callback_worker_org4(org4):
+            if org4.text == 'лингвистические опросы':
+                r4 = bot.send_message(org4.chat.id, 'Да!')
+                markup = types.ReplyKeyboardMarkup(False, True)
+                itembtna = types.KeyboardButton('лингвисты 2020')
+                itembtnv = types.KeyboardButton('лесбисты(-ки)’20')
+                itembtnc = types.KeyboardButton('лингвистки’20')
+                itembtnd = types.KeyboardButton('лесбистки(-ы)’20')
+                markup.row(itembtna, itembtnv)
+                markup.row(itembtnc, itembtnd)
+                bot.send_message(org4.chat.id, "Как называется беседа потока?", reply_markup=markup)
+                bot.register_next_step_handler(r4, callback_worker_org5)
+
+
+            else:
+                r4 = bot.send_message(org4.chat.id, 'Нет( \nправильный ответ: лингвистические опросы')
+                markup = types.ReplyKeyboardMarkup(False, True)
+                itembtna = types.KeyboardButton('лингвисты 2020')
+                itembtnv = types.KeyboardButton('лесбисты(-ки)’20')
+                itembtnc = types.KeyboardButton('лингвистки’20')
+                itembtnd = types.KeyboardButton('лесбистки(-ы)’20')
+                markup.row(itembtna, itembtnv)
+                markup.row(itembtnc, itembtnd)
+                bot.send_message(org4.chat.id, "Как называется беседа потока?", reply_markup=markup)
+                bot.register_next_step_handler(r4, callback_worker_org5)
+
         def callback_worker_org3(org3):
             if org3.text == '4':
-                bot.send_message(org3.chat.id, 'Да!')
+                r3 = bot.send_message(org3.chat.id, 'Да!')
+                markup = types.ReplyKeyboardMarkup(False, True)
+                itembtna = types.KeyboardButton('мемы')
+                itembtnv = types.KeyboardButton('музыку')
+                itembtnc = types.KeyboardButton('фотографии Ландера')
+                itembtnd = types.KeyboardButton('лингвистические опросы')
+                itembtne = types.KeyboardButton('скрины писем из учебного офиса')
+                markup.row(itembtna, itembtnv, itembtnc)
+                markup.row(itembtnd, itembtne)
+                bot.send_message(org3.chat.id, "Что отправляют в беседу чаще всего?", reply_markup=markup)
+                bot.register_next_step_handler(r3, callback_worker_org4)
             else:
-                bot.send_message(org3.chat.id, 'Нет(')
+                r3 = bot.send_message(org3.chat.id, 'Нет( \nправильный ответ: 4')
+                markup = types.ReplyKeyboardMarkup(False, True)
+                itembtna = types.KeyboardButton('мемы')
+                itembtnv = types.KeyboardButton('музыку')
+                itembtnc = types.KeyboardButton('фотографии Ландера')
+                itembtnd = types.KeyboardButton('лингвистические опросы')
+                itembtne = types.KeyboardButton('скрины писем из учебного офиса')
+                markup.row(itembtna, itembtnv, itembtnc)
+                markup.row(itembtnd, itembtne)
+                bot.send_message(org3.chat.id, "Что отправляют в беседу чаще всего?", reply_markup=markup)
+                bot.register_next_step_handler(r3, callback_worker_org4)
 
         def callback_worker_org2(org2):
             if org2.text == 'Юрий Александрович':
@@ -90,7 +144,7 @@ def callback_worker(call):
                 bot.send_message(org2.chat.id, "На каком этаже находится учебный офис ФиКЛ? Введите число")
                 bot.register_next_step_handler(r2, callback_worker_org3)
             elif org2.text == 'Михаил Александрович' or org2.text == 'Владимир Владимирович' or org2.text == 'Андриан Викторович':
-                r2 = bot.send_message(org2.chat.id, 'Нет(')
+                r2 = bot.send_message(org2.chat.id, 'Нет( \nправильный ответ: Юрий Александрович')
                 bot.send_message(org2.chat.id, "На каком этаже находится учебный офис ФиКЛ? Введите число")
                 bot.register_next_step_handler(r2, callback_worker_org3)
 
